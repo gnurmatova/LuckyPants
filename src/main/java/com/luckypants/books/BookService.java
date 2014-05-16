ea22a343a5ab6e59a6369e530b3e0bfbff7096c9
gnurmatova <gulanurmatova@Gulas-MacBook-Pro.local>
2014-05-16 16:52:06 -0400

    properties

--- src/main/java/com/luckypants/books/BookService.java
+++ src/main/java/com/luckypants/books/BookService.java
@@ -2,14 +2,17 @@ package com.luckypants.books;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
@@ -22,6 +25,7 @@ import com.luckypants.command.GetBookCommand;
 import com.luckypants.command.ListAllBooksCommand;
 import com.luckypants.command.ProvidePackagedFileCommand;
 import com.luckypants.model.Book;
+import com.luckypants.properties.PropertiesLookup;
 import com.mongodb.DBObject;
 
 @Path("/books")
@@ -37,6 +41,15 @@ public class BookService {
 	}
 
 	@GET
+	@Path("properties/{property}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response propertiesCheck(@PathParam("property") String property) {
+		PropertiesLookup pl = new PropertiesLookup();
+		String pValue = pl.getProperty(property);
+		return Response.status(200).entity(pValue).build();
+	}
+
+	@GET
 	@Path("/{isbn}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getBook(@PathParam("isbn") String isbn) {
@@ -47,7 +60,7 @@ public class BookService {
 
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN })
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN })
 	public Response createBook(String bookStr) {
 
 		try {
@@ -63,7 +76,26 @@ public class BookService {
 			return Response.status(500).entity(e.toString()).build();
 		}
 	}
-	
+
+	@GET
+	@Path("/search")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response searchBook(
+			@DefaultValue("*") @QueryParam("query") String query,
+			@DefaultValue("author") @QueryParam("sortby") String sortby) {
+		HashMap<String, String> responseMap = new HashMap<String, String>();
+		responseMap.put("query", query);
+		responseMap.put("sortby", sortby);
+		String rString = "";
+		try {
+			rString = mapper.writeValueAsString(responseMap);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity(e.toString()).build();
+		}
+		return Response.status(200).entity(rString).build();
+	}
+
 	@DELETE
 	@Path("/{isbn}")
 	public Response deleteBook(@PathParam("isbn") String isbn) {
@@ -71,15 +103,14 @@ public class BookService {
 		delete.execute(isbn);
 		return Response.status(200).build();
 	}
-	
+
 	@GET
 	@Path("files/{filename}")
 	@Produces(MediaType.WILDCARD)
 	public Response getFile(@PathParam("filename") String filename) {
 		try {
 			ProvidePackagedFileCommand getFile = new ProvidePackagedFileCommand();
-			InputStream is =  getFile.execute(filename);
-			
+			InputStream is = getFile.execute(filename);
 
 			ResponseBuilder response = Response.ok((Object) is);
 			response.header("Content-Disposition", "attachment; filename=\""
@@ -89,16 +120,14 @@ public class BookService {
 			return Response.status(404).entity(e.getMessage()).build();
 		}
 	}
-	
-	
+
 	@GET
 	@Path("inline/{filename}")
 	@Produces(MediaType.WILDCARD)
 	public Response renderFile(@PathParam("filename") String filename) {
 		try {
 			ProvidePackagedFileCommand getFile = new ProvidePackagedFileCommand();
-			InputStream is =  getFile.execute(filename);
-			
+			InputStream is = getFile.execute(filename);
 
 			ResponseBuilder response = Response.ok((Object) is);
 			response.header("Content-Disposition", "inline; filename=\""
